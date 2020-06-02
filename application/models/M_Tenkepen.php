9739465c234e824a2628f5ffc7b438485ee0ebd0
rnggara <ranggaanggara8@gmail.com>
2020-06-02 23:17:17 +0700

    update 2 juni 2020

--- application/models/M_Tenkepen.php
+++ application/models/M_Tenkepen.php
@@ -19,6 +19,17 @@ class M_Tenkepen extends CI_Model
 		$this->db->where('status', 2);
 		return $this->db->get('sia_tenkepen');
 	}
+
+	function add_tenkepen($data){
+		$this->db->insert('sia_tenkepen', $data);
+	}
+
+	function delete($id){
+		$data = array(
+			'status' => 0);
+		$this->db->where('id_tenkepen', $id);
+		$this->db->update('sia_tenkepen');
+	}
 }
 
  ?>
